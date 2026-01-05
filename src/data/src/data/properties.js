7b865c1007c1819fe69f77094c393aa806a5cf55
Arpit Chaudhary <133642488+iamarpitchaudhary@users.noreply.github.com>
2026-01-05 17:33:40 +0530

    properties.js

---
+++ src/data/src/data/properties.js
@@ -0,0 +1,30 @@
+// All prices in Lakhs
+export const properties = [
+  {
+    id: 1,
+    name: "LID Nivasa",
+    location: "Sohna Sector 6",
+    bhk: 2,
+    sizeSqYd: 125,
+    priceLakhs: 110, // 1.10 Crore
+    possession: "2027",
+  },
+  {
+    id: 2,
+    name: "LID Nivasa",
+    location: "Sohna Sector 6",
+    bhk: 3,
+    sizeSqYd: 155,
+    priceLakhs: 145, // 1.45 Crore
+    possession: "2027",
+  },
+  {
+    id: 3,
+    name: "ABC Residency",
+    location: "Gurgaon Sector 45",
+    bhk: 2,
+    sizeSqYd: 130,
+    priceLakhs: 125,
+    possession: "2026",
+  }
+];
